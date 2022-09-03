8d486fe383739c420eae86a2f1cd1162ae110aa2
johanrdev <johanrothman1988@gmail.com>
2022-09-03 14:33:35 +0200

    Implemented dashboard sidebar and sideviews

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,24 +7,51 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 grid grid-cols-12 gap-6">
-            <div class="overflow-hidden col-span-3">
-                <!-- Sidebar section -->
-                <div class="p-6 bg-white border-b border-gray-200 mb-6">
-                    <h2 class="pb-3 text-center font-bold">Dashboard</h2>
-                    <ul>
-                        <li class="flex border-b hover:bg-green-100 transition-colors duration-250"><a href="?tab=blogs" class="grow p-2">Blogs</a></li>
-                        <li class="flex border-b hover:bg-green-100 transition-colors duration-250"><a href="?tab=posts" class="grow p-2">Posts</a></li>
-                        <li class="flex border-b hover:bg-green-100 transition-colors duration-250"><a href="?tab=categories" class="grow p-2">Categories</a></li>
-                        <li class="flex border-b hover:bg-green-100 transition-colors duration-250"><a href="?tab=comments" class="grow p-2">Comments</a></li>
-                        <li class="flex hover:bg-green-100 transition-colors duration-250"><a href="?tab=bookmarks" class="grow p-2">Bookmarks</a></li>
-                    </ul>
+            <div class="col-span-3 flex flex-col">
+                <!-- Sidebar section start -->
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <h2 class="pb-3 text-center font-bold">Dashboard</h2>
+                        <ul>
+                            <li class="flex border-b hover:bg-green-100 transition-colors duration-250"><a href="?tab=blogs" class="grow p-2">Blogs</a></li>
+                            <li class="flex border-b hover:bg-green-100 transition-colors duration-250"><a href="?tab=posts" class="grow p-2">Posts</a></li>
+                            <li class="flex border-b hover:bg-green-100 transition-colors duration-250"><a href="?tab=categories" class="grow p-2">Categories</a></li>
+                            <li class="flex border-b hover:bg-green-100 transition-colors duration-250"><a href="?tab=comments" class="grow p-2">Comments</a></li>
+                            <li class="flex hover:bg-green-100 transition-colors duration-250"><a href="?tab=bookmarks" class="grow p-2">Bookmarks</a></li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
 
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg col-span-9">
-                <div class="p-6 bg-white border-b border-gray-200">
+                {{-- <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-6">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <h2 class="pb-3 text-center font-bold">Dashboard</h2>
+                        <ul>
+                            <li class="flex border-b hover:bg-green-100 transition-colors duration-250"><a href="?tab=blogs" class="grow p-2">Blogs</a></li>
+                            <li class="flex border-b hover:bg-green-100 transition-colors duration-250"><a href="?tab=posts" class="grow p-2">Posts</a></li>
+                            <li class="flex border-b hover:bg-green-100 transition-colors duration-250"><a href="?tab=categories" class="grow p-2">Categories</a></li>
+                            <li class="flex border-b hover:bg-green-100 transition-colors duration-250"><a href="?tab=comments" class="grow p-2">Comments</a></li>
+                            <li class="flex hover:bg-green-100 transition-colors duration-250"><a href="?tab=bookmarks" class="grow p-2">Bookmarks</a></li>
+                        </ul>
+                    </div>
+                </div> --}}
+                <!-- Sidebar section end -->
+            </div>
+            
+
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg col-span-9 flex flex-col">
+                <div class="p-6 bg-white border-b border-gray-200 grow">
                     
-                    @include('dashboard.blogs')
+                    @if (request('tab') == 'blogs')
+                        @include('dashboard.blogs')
+                    @elseif (request('tab') == 'posts')
+                        @include('dashboard.posts')
+                    @elseif (request('tab') == 'categories')
+                        @include('dashboard.categories')
+                    @elseif (request('tab') == 'comments')
+                        @include('dashboard.comments')
+                    @elseif (request('tab') == 'bookmarks')
+                        @include('dashboard.bookmarks')
+                    @endif
                     
                 </div>
             </div>
