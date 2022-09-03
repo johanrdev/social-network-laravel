59a88de7140c4ba24274472a89ba5fdd867bf505
johanrdev <johanrothman1988@gmail.com>
2022-09-03 13:51:31 +0200

    Added table to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,27 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    
+                    
+                    <table class="w-full text-left">
+                        <thead class="bg-gray-200">
+                            <tr>
+                                <th class="py-3 px-6">Name</th>
+                                <th class="py-3 px-6">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="border-b">
+                                <td class="py-3 px-6">Test</td>
+                                <td class="py-3 px-6">
+                                    <a href="#">Edit</a>
+                                    <a href="#">Remove</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+
                 </div>
             </div>
         </div>
