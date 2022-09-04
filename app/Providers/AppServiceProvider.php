9aa0ee7cdc16306ce4b3d7e6b2453e94664a7eb3
johanrdev <johanrothman1988@gmail.com>
2022-09-04 13:28:14 +0200

    Implemented select blog in sidebar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Blog;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('*', function($view) {
+            if (!Auth::check()) return;
+
+            $blogs = Blog::where('user_id', Auth::user()->id)->orderBy('id', 'desc')->get();
+            $view->with('userBlogs', $blogs);
+        });
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,19 +3,34 @@
 @section('container')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 grid grid-cols-12 gap-6">
-            
-            <x-sidebar :title="'Dashboard'" :sections="[
-                [
-                    'title' => 'Dashboard',
-                    'links' => [
-                        'Blogs' => '?tab=blogs',
-                        'Posts' => '?tab=posts',
-                        'Categories' => '?tab=categories',
-                        'Comments' => '?tab=comments',
-                        'Bookmarks' => '?tab=bookmarks'
+            <div class="col-span-3 flex flex-col">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-6 last:mb-0">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <h2 class="pb-3 text-center font-bold">Select Blog</h2>
+                        <select class="w-full mb-3">
+                            @foreach ($userBlogs as $blog)
+                                <option value="{{ $blog->id }}">{{ $blog->name }}</option>
+                            @endforeach
+                        </select>
+                        <div class="flex justify-end">
+                            <input type="button" class="py-2 px-4 rounded bg-green-500 text-white cursor-pointer ml-3" value="Select" />
+                        </div>
+                    </div>
+                </div>
+                
+                <x-sidebar :title="'Dashboard'" :sections="[
+                    [
+                        'title' => 'Dashboard',
+                        'links' => [
+                            'Blogs' => '?tab=blogs',
+                            'Posts' => '?tab=posts',
+                            'Categories' => '?tab=categories',
+                            'Comments' => '?tab=comments',
+                            'Bookmarks' => '?tab=bookmarks'
+                        ]
                     ]
-                ]
-            ]" />
+                ]" />
+            </div>
 
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg col-span-9 flex flex-col">
                 <div class="p-6 bg-white border-b border-gray-200 grow">
