20df0cb5fdbaf7d93991030c46f672ca313e0c66
johanrdev <johanrothman1988@gmail.com>
2022-09-06 18:42:12 +0200

    Removed top padding on small devices on individual post page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('container')
-    <div class="py-12">
+    <div class="pb-12 sm:py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg grow flex flex-col mb-3">
                 <div class="bg-white border-b border-gray-200 grow">
