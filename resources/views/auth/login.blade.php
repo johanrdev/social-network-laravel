8c61d17153c3b4849b9028f994e0b21c0895cb54
johanrdev <johanrothman1988@gmail.com>
2022-09-07 22:20:06 +0200

    Styled the login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,10 +7,10 @@
         </x-slot>
 
         <div class="flex justify-between items-center py-3">
-            <h2 class="text-2xl font-bold">Login</h2>
+            <h2 class="text-2xl font-bold text-gray-400">Login</h2>
             <ul>
                 <li>
-                    <a href="#" class="transition-all duration-150 cursor-pointer font-black text-sm uppercase tracking-widest text-teal-500 hover:text-teal-700 underline">Go back</a>
+                    <a href="{{ URL::previous() }}" class="transition-all duration-150 cursor-pointer font-black text-sm uppercase tracking-widest text-teal-500 hover:text-teal-700 underline">Go back</a>
                 </li>
             </ul>
         </div>
@@ -36,22 +36,22 @@
                 <x-label for="password" :value="__('Password')" />
 
                 <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+                         type="password"
+                         name="password"
+                         required autocomplete="current-password" />
             </div>
 
             <!-- Remember Me -->
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-gray-400 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                    <span class="ml-2 text-sm text-gray-400 font-semibold">{{ __('Remember me') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-400 hover:text-gray-200 font-semibold" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
