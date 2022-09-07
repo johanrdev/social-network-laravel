9fd888bfe2c2f9c6a25b031793707fba475e5f48
johanrdev <johanrothman1988@gmail.com>
2022-09-07 22:11:21 +0200

    Styled welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,15 +19,6 @@
         <div class="min-h-screen bg-gray-200">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            {{-- <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                        @yield('title')
-                    </h2>
-                </div>
-            </header> --}}
-
             <!-- Page Content -->
             <main>
                 @yield('container')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,33 +19,38 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <svg class="w-10 h-10" fill="none" stroke="gray" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <div class="relative flex items-top justify-center min-h-screen bg-gradient-to-b from-gray-700 to-black dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 flex flex-col">
+                <div class="flex flex-col items-center justify-center pt-8 sm:pt-0 stroke-gray-500">
+                    <svg class="w-40 h-40" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5"></path>
                     </svg>
-                    Headline
+                    <span class="text-3xl sm:text-4xl text-gray-500 font-black">Welcome to TextBox</span>
                 </div>
 
                 @if (Route::has('login'))
-                    <div class="top-0 right-0 px-6 py-4 sm:block">
+                    <div class="top-0 right-0 px-6 py-4 flex justify-center">
                         @auth
-                            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                            <a href="{{ url('/dashboard') }}" class="text-xs text-gray-200 dark:text-gray-500 uppercase font-semibold tracking-wides">Dashboard</a>
                         @else
-                            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                            <a href="{{ route('login') }}" class="text-xs text-gray-400 dark:text-gray-500 uppercase font-semibold tracking-widest">Log in</a>
 
                             @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                                <a href="{{ route('register') }}" class="ml-4 text-xs text-gray-400 dark:text-gray-500 uppercase font-semibold tracking-widest">Register</a>
                             @endif
                         @endauth
                     </div>
                 @endif
 
+                <div class="grow"></div>
+
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
-                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                    <div class="text-center font-semibold text-md text-gray-400 sm:ml-0 grow">
                         &copy; Copyright {{ date('Y') }}
                     </div>
                 </div>
