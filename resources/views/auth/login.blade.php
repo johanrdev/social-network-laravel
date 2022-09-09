863455a2aa70fe73401b57df3eefe16ba7e33721
johanrdev <johanrothman1988@gmail.com>
2022-09-09 22:10:29 +0200

    Added alert component to login and register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
         @if ($errors->any())
             <ul>
                 @foreach ($errors->all() as $error)
-                    <x-alert :message="$error"></x-alert>
+                    <x-alert :message="$error" :type="'danger'"></x-alert>
                 @endforeach
             </ul>
         @endif
