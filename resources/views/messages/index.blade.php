012f1c3af0fdde8a471ecaa73803b6a71c34760c
johanrdev <johanrothman1988@gmail.com>
2022-09-09 23:43:23 +0200

    Show 'no messages' if no messages were found

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -11,30 +11,40 @@
             @endif
 
             <div>
-                @foreach ($messages as $message)
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg grow flex flex-col mb-6">
-                    <div class="bg-white border-b border-gray-200 grow flex flex-col">
-            
-                        <div class="p-6 grow flex flex-col">
-                            <div class="flex">
-                                <div class="bg-gray-800 rounded-full w-20 h-20 mr-6 shrink-0 self-center flex flex-col items-center justify-center">
-                                    <span class="uppercase text-gray-600 font-bold text-3xl">U</span>
-                                </div>
-                                <div class="flex flex-col grow">
-                                    <h2 class="text-xl font-bold">
-                                        <a href="{{ route('users.show', $message->sender_id) }}">{{ $message->sender->name }}</a>
-                                    </h2>
-                                    <ul class="flex">
-                                        <li class="mr-4">{{ $message->created_at->diffForHumans() }}</li>
-                                        <li class="mr-4"><a href="{{ route('messages.create') }}?recipient_id={{ $message->sender_id }}" class="font-bold">Reply</a></li>
-                                    </ul>
-                                    <p>{{ $message->content }}</p>
+                @if (count($messages) > 0)
+                    @foreach ($messages as $message)
+                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg grow flex flex-col mb-6">
+                            <div class="bg-white border-b border-gray-200 grow flex flex-col">
+                    
+                                <div class="p-6 grow flex flex-col">
+                                    <div class="flex">
+                                        <div class="bg-gray-800 rounded-full w-20 h-20 mr-6 shrink-0 self-center flex flex-col items-center justify-center">
+                                            <span class="uppercase text-gray-600 font-bold text-3xl">U</span>
+                                        </div>
+                                        <div class="flex flex-col grow">
+                                            <h2 class="text-xl font-bold">
+                                                <a href="{{ route('users.show', $message->sender_id) }}">{{ $message->sender->name }}</a>
+                                            </h2>
+                                            <ul class="flex">
+                                                <li class="mr-4">{{ $message->created_at->diffForHumans() }}</li>
+                                                <li class="mr-4"><a href="{{ route('messages.create') }}?recipient_id={{ $message->sender_id }}" class="font-bold">Reply</a></li>
+                                            </ul>
+                                            <p>{{ $message->content }}</p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                    @endforeach
+                @else 
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg grow flex flex-col mb-3">
+                        <div class="bg-white border-b border-gray-200 grow grid grid-cols-1 lg:grid-cols-3">
+                            <div class="flex flex-col p-6 col-span-2">
+                                <p>No messages</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                @endforeach
+                @endif
             </div>
 
             @if ($messages->hasPages())
