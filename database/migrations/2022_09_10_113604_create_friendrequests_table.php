cd0556a4a537f9986123b6295c53ddff8a72b45b
johanrdev <johanrothman1988@gmail.com>
2022-09-10 14:01:12 +0200

    Created friend_requests table

---
+++ database/migrations/2022_09_10_113604_create_friendrequests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friend_requests', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('from_id');
+            $table->unsignedBigInteger('to_id');
+            $table->foreign('from_id')->references('id')->on('users');
+            $table->foreign('to_id')->references('id')->on('users');
+            $table->boolean('is_accepted')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friendrequests');
+    }
+};
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -28,7 +28,7 @@
                     <x-nav-link :href="route('explore')" :active="request()->routeIs('explore')">
                         <svg class="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path></svg>
                         @if (request()->routeIs('explore'))
-                            {{ __('Explore') }}
+                            {{ __('Browse') }}
                         @endif
                     </x-nav-link>
 
@@ -47,7 +47,7 @@
                         @endif
 
                         @if ($new_messages > 0)
-                            <span class="bg-rose-500 rounded-sm font-semibold px-1 uppercase text-xs text-white pointer-events-none select-none absolute -right-2 top-4">
+                            <span class="bg-rose-500 rounded-sm font-semibold px-1 uppercase text-xs text-white pointer-events-none select-none absolute right-0 top-0">
                                 {{ $new_messages }}
                             </span>
                         @endif
@@ -126,10 +126,10 @@
                     {{ __('Messages') }}
 
                     @if ($new_messages > 0)
-                            <span class="bg-rose-500 rounded-sm font-semibold px-1 uppercase text-xs text-white pointer-events-none select-none ml-2">
-                                {{ $new_messages }}
-                            </span>
-                        @endif
+                        <span class="bg-rose-500 rounded-sm font-semibold px-1 uppercase text-xs text-white pointer-events-none select-none ml-2">
+                            {{ $new_messages }}
+                        </span>
+                    @endif
                 </x-responsive-nav-link>
             </div>
 
