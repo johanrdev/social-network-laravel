070e1c19c6565bf9edd5e357f5e20f4299375f2c
johanrdev <johanrothman1988@gmail.com>
2022-09-10 13:15:02 +0200

    Restyled the navbar links

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,10 +2,10 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-4 pt-1 border-b-4 border-indigo-400 text-md font-semibold leading-5 text-gray-200 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out bg-gray-600'
-            : 'inline-flex items-center px-4 pt-1 border-b-4 border-transparent text-md font-semibold leading-5 text-gray-400 hover:text-gray-200 hover:border-indigo-200 focus:outline-none focus:text-gray-200 focus:border-indigo-200 transition duration-150 ease-in-out';
+            ? 'border-indigo-400 text-gray-200 focus:outline-none focus:border-indigo-700 bg-gray-600'
+            : 'border-transparent text-gray-400 hover:text-gray-200 hover:border-indigo-200 focus:outline-none focus:text-gray-200 focus:border-indigo-200';
 @endphp
 
-<a {{ $attributes->merge(['class' => $classes]) }}>
+<a {{ $attributes->merge(['class' => 'inline-flex items-center px-4 pt-1 border-b-4 uppercase leading-5 text-xs tracking-widest font-bold transition duration-150 ease-in-out mt-2 pb-2 rounded-t ' . $classes]) }}>
     {{ $slot }}
 </a>
